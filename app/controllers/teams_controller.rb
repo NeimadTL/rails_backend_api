a376e58d228b1dce1b355e8dae736dc3e2e96d6f
neimad971 <damientalbot26@gmail.com>
2018-06-09 18:35:50 +0200

    active model serializer has been added so it's easy to 'format' the api

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,11 +2,12 @@ class TeamsController < ApplicationController
 
   def index
     @teams = Team.all
-    render json: @teams,
-      include: {
-        :manager => {:only => :full_name},
-        :members => {:only => :full_name}
-      },
-      except: [:id, :manager_id, :created_at, :updated_at]
+    render json: @teams
+    # render json: @teams,
+    #   include: {
+    #     :manager => {:only => :full_name},
+    #     :members => {:only => :full_name}
+    #   },
+    #   except: [:id, :manager_id, :created_at, :updated_at]
   end
 end
---
+++ app/serializers/team_serializer.rb
@@ -0,0 +1,15 @@
+class TeamSerializer < ActiveModel::Serializer
+  attributes :name, :manager, :members
+
+  def manager
+    object.manager.full_name
+  end
+
+  def members
+    members_array = []
+    object.members.each do |member|
+      members_array << member.full_name
+    end
+    members_array
+  end
+end
