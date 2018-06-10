10ed961fef0b3026c5716441041ba3dc1483d35c
neimad971 <damientalbot26@gmail.com>
2018-06-10 19:21:22 +0200

    A user can be added to a team by now; I've had to include 'rack-cors' gem

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,11 +3,5 @@ class TeamsController < ApplicationController
   def index
     @teams = Team.all
     render json: @teams
-    # render json: @teams,
-    #   include: {
-    #     :manager => {:only => :full_name},
-    #     :members => {:only => :full_name}
-    #   },
-    #   except: [:id, :manager_id, :created_at, :updated_at]
   end
 end
